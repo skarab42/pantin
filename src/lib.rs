ae12fdef05f43791a82ae10d5e61eae2abc4c151
skarab42 <skarab@bluewin.ch>
2025-02-09 11:05:55 +0100

    feat: clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+#![deny(clippy::unwrap_used, clippy::expect_used, clippy::panic)]
+#![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
+#![allow(clippy::missing_errors_doc)]
+
 pub fn hello_world() {
     println!("Tchao pantin!");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#![deny(clippy::unwrap_used, clippy::expect_used, clippy::panic)]
+#![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
+#![allow(clippy::multiple_crate_versions)]
+
 fn main() {
     pantin_lib::hello_world();
 }
