f6f0fb453b94162cc8e01dc34916d0fcedc82e7e
skarab42 <skarab@bluewin.ch>
2025-03-02 15:45:22 +0100

    chore: remove allow `clippy::missing_errors_doc` rule

--- crates/marionette/src/lib.rs
+++ crates/marionette/src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(clippy::unwrap_used, clippy::expect_used, clippy::panic)]
 #![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
-#![allow(clippy::missing_errors_doc, clippy::multiple_crate_versions)]
+#![allow(clippy::multiple_crate_versions)]
 #![cfg_attr(coverage, feature(coverage_attribute))]
 
 //! Crate for controlling a Marionette client that communicates with a Marionette server.
